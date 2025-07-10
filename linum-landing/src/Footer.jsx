10a2378cab37a92206e396e20403824ccf7d3d13
NataliiaSwan <shapnat69@gmail.com>
2025-07-10 22:05:59 +0300

    update footer

--- linum-landing/src/Footer.jsx
+++ linum-landing/src/Footer.jsx
@@ -1,14 +1,53 @@
+// import React from "react";
+// import css from "./Footer.module.css";
+// import { FaInstagram, FaFacebookF, FaTelegramPlane } from "react-icons/fa";
+
+// const Footer = () => {
+//   return (
+//     <footer id="footer" className={css.footer}>
+//       <div className={css.content}>
+//         <p className={css.text}>
+//           © {new Date().getFullYear()} Linum. Усі права захищені.
+//         </p>
+//         <div className={css.socialIcons}>
+//           <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+//             <FaInstagram />
+//           </a>
+//           <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+//             <FaFacebookF />
+//           </a>
+//           <a href="https://t.me/yourchannel" target="_blank" rel="noreferrer">
+//             <FaTelegramPlane />
+//           </a>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
 import React from "react";
 import css from "./Footer.module.css";
 import { FaInstagram, FaFacebookF, FaTelegramPlane } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className={css.footer}>
+    <footer id="footer" className={css.footer}>
       <div className={css.content}>
-        <p className={css.text}>
-          © {new Date().getFullYear()} Linum. Усі права захищені.
-        </p>
+        <div className={css.contacts}>
+          <p>
+            <strong>Контакти:</strong>
+          </p>
+          <p>
+            Телефон: <a href="tel:+380971234567">+38 (097) 123 45 67</a>
+          </p>
+          <p>
+            Email: <a href="mailto:info@linum.ua">info@linum.ua</a>
+          </p>
+          <p>Адреса: вул. Прикладна, 12, Київ, Україна</p>
+        </div>
+
         <div className={css.socialIcons}>
           <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
             <FaInstagram />
@@ -20,6 +59,10 @@ const Footer = () => {
             <FaTelegramPlane />
           </a>
         </div>
+
+        <p className={css.text}>
+          © {new Date().getFullYear()} Linum. Усі права захищені.
+        </p>
       </div>
     </footer>
   );
